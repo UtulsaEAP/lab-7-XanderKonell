90d7cb8a329d51cc3575f9a95cc1c8bee71842b5
xander konell <xanderkonell@gmail.com>
2024-03-06 15:02:36 -0600

    First 2 problems done

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -2,6 +2,22 @@
 #Hour: 3
 def wordInRange():
     #Type your code here
+    file_name = input().strip()
+    lower_bound = input().strip()
+    upper_bound = input().strip()
+    
+    with open(file_name, 'r') as file:
+        words = file.readlines()
+    
+    words.sort()
+    
+    for word in words:
+        word = word.strip()
+        if lower_bound <= word <= upper_bound:
+            print(f'{word} - in range')
+        else:
+            print(f'{word} - not in range')
+            
     return
 if __name__ == '__main__':
     wordInRange()
