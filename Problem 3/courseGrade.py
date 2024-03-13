31e98f7835645e10460e335814cf29bcbba175ed
xander konell <xanderkonell@gmail.com>
2024-03-13 14:12:57 -0500

    Question 3 and 4 progress

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,15 +1,53 @@
 #Name: Xander Konell
 #Hour: 3
-def courseGrade():
+def courseGrade(score):
     
     # TODO: Declare any necessary variables here. 
-      
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
       
     # TODO: Read a file name from the user and read the tsv file here. 
-   
+file_name = input("")
+with open(file_name, 'r') as file:
+    lines = file.readlines()
    
     # TODO: Compute student grades and exam averages, then output results to a text file here. 
-    return
+midterm1_total = 0
+midterm2_total = 0
+final_total = 0
+with open('report.txt', 'w') as report_file:
+    for line in lines:
+        data = line.split('\t')
+       
+        last_name = data[0]
+        first_name = data[1]
+       
+        midterm1 = int(data[2])
+        midterm2 = int(data[3])
+        final = int(data[4])
+      
+        avg_score = (midterm1 + midterm2 + final) / 3
+        letter_grade = compute_grade(avg_score)
+        report_file.write(f"{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{letter_grade}\n")
+      
+        midterm1_total += midterm1
+        midterm2_total += midterm2
+        final_total += final
+    
+    num_students = len(lines)
+    midterm1_avg = midterm1_total / num_students
+    midterm2_avg = midterm2_total / num_students
+    final_avg = final_total / num_students
+    
+    report_file.write(f"\nAverages: midterm1 {midterm1_avg:.2f}, midterm2 {midterm2_avg:.2f}, final {final_avg:.2f}\n")
 
 if __name__ == "__main__":
     courseGrade()
--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -2,7 +2,16 @@
 #Hour: 3
 def fileNameChange():
     #type your code here
+    input_file_name = input("")
 
+    try:
+        with open(input_file_name, 'r') as file:
+            for line in file:
+                photo_file_name = line.strip()
+                modified_file_name = photo_file_name.replace("_photo.jpg", "_info.txt")
+                print(modified_file_name)
+    except FileNotFoundError:
+        print("File not found!")
     return
 
 if __name__ == '__main__':
